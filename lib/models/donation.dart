af0323be3729039021a73b0543022c5f62accf32
Casey Nguyen <cxn220034@utdallas.edu>
2025-04-06 18:47:44 -0500

    first commit

---
+++ lib/models/donation.dart
@@ -0,0 +1,27 @@
+class Donation {
+  final String name;
+  final String type;
+  final String detail;
+
+  Donation({
+    required this.name,
+    required this.type,
+    required this.detail,
+  });
+
+  factory Donation.fromJson(Map<String, dynamic> json) {
+    return Donation(
+      name: json['name'],
+      type: json['type'],
+      detail: json['detail'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'type': type,
+        'detail': detail,
+      };
+}
+
+
